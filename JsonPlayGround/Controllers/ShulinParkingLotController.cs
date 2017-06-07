26ff8ea81d512eacf4c32da3143b7435b7d1171c
Sha-Pai LI <lisa0621@gmail.com>
2017-06-07 11:48:12 +0800

    Cache Runtime

--- JsonPlayGround/Controllers/ShulinParkingLotController.cs
+++ JsonPlayGround/Controllers/ShulinParkingLotController.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Runtime.Caching;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
@@ -15,6 +16,30 @@ namespace JsonPlayGround.Controllers
     {
         // GET: ShulinParkingLot
         public async Task<ActionResult> Index()
+        {
+            var lotSource = await this.GetShulinParkingLotData();
+            ViewData.Model = lotSource;
+            return View();
+        }
+
+        private async Task<IEnumerable<ShulinParkingLotVM>> GetShulinParkingLotData()
+        {
+            string cacheName = "Shulin_ParkingLot";
+
+            ObjectCache cache = MemoryCache.Default;
+            CacheItem cacheContents = cache.GetCacheItem(cacheName);
+
+            if (cacheContents == null)
+            {
+                return await RetriveShulinParkingLotData(cacheName);
+            }
+            else
+            {
+                return cacheContents.Value as IEnumerable<ShulinParkingLotVM>;
+            }
+        }
+
+        private async Task<IEnumerable<ShulinParkingLotVM>> RetriveShulinParkingLotData(string cacheName)
         {
             string targetURI = "http://data.ntpc.gov.tw/api/v1/rest/datastore/382000000A-G00218-002";
 
@@ -27,9 +52,15 @@ namespace JsonPlayGround.Controllers
             // get JSON result objects into a list
             string results = resultJO["result"]["records"].ToString();
 
-            List<ShulinParkingLotVM> collection = JsonConvert.DeserializeObject<List<ShulinParkingLotVM>>(results);
-            
-            return View(collection);
+            IEnumerable<ShulinParkingLotVM> collection = JsonConvert.DeserializeObject<IEnumerable<ShulinParkingLotVM>>(results);
+
+            CacheItemPolicy policy = new CacheItemPolicy();
+            policy.AbsoluteExpiration = DateTime.Now.AddMinutes(30);
+
+            ObjectCache cacheItem = MemoryCache.Default;
+            cacheItem.Add(cacheName, collection, policy);
+
+            return collection;
         }
-    }
+    }    
 }
